b3422206cd5db818c054293d849ddcfd37ff7db9
ilyes <ilyes38400@gmail.com>
2022-05-21 13:47:39 +0200

    fix server

--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 var app_path ='../dist/ilyes-front';
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   next();
-})
+});
 
 app.use('/',express.static(path.join(__dirname,app_path)))
   .get('*',(req,res)=> res.sendFile(path.join(__dirname,app_path+'/index.html')))
