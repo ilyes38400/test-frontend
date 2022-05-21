0cfe808459f209af9c4b81c0c00a7f9b390f4a8a
ilyes <ilyes38400@gmail.com>
2022-05-21 13:43:12 +0200

    fix server

--- server/server.js
+++ server/server.js
@@ -4,16 +4,13 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 var app_path ='../dist/ilyes-front';
 
-
-app.use('/',express.static(path.join(__dirname,app_path)))
-  .get('*',(req,res)=> res.sendFile(path.join(__dirname,app_path+'/index.html')))
-  .listen(PORT,()=>console.log(`listen on ${PORT}` ))
-
-
-
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 })
+
+app.use('/',express.static(path.join(__dirname,app_path)))
+  .get('*',(req,res)=> res.sendFile(path.join(__dirname,app_path+'/index.html')))
+  .listen(PORT,()=>console.log(`listen on ${PORT}` ))
