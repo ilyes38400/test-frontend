e2b3d9c09e4b52fceb82011ea3b638dda370a120
ilyes <ilyes38400@gmail.com>
2022-05-21 13:09:43 +0200

    autorise access control

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 var app_path ='../dist/ilyes-front';
 
+
 app.use('/',express.static(path.join(__dirname,app_path)))
   .get('*',(req,res)=> res.sendFile(path.join(__dirname,app_path+'/index.html')))
   .listen(PORT,()=>console.log(`listen on ${PORT}` ))
+
+pp.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
